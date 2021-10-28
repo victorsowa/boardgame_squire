4fd5b124f06fad227b7ed9edd63ec61e59db3c68
Victor Sowa <victor.sowa@etraveligroup.com>
2021-10-28 14:22:34 +0200

    Turn off sqlalchemy echoing

--- boardgames/data/db_session.py
+++ boardgames/data/db_session.py
@@ -18,9 +18,7 @@ def global_init(db_file: str):
     conn_str = "sqlite:///" + db_file.strip()
     print("Connecting to DB with {}".format(conn_str))
 
-    engine = sa.create_engine(
-        conn_str, echo=True, connect_args={"check_same_thread": False}
-    )
+    engine = sa.create_engine(conn_str, connect_args={"check_same_thread": False})
     __factory = orm.sessionmaker(bind=engine)
 
     import boardgames.data.__all_models
